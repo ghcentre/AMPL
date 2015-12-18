dbde31c1b01dae18eba92e5513ca01578610f6d1
ghcentre <ghcentre@gmail.com>
2015-12-18 15:07:20 +0300

    Added: DictionaryExtensions.GetValyeOrDefault

--- Ampl.Core/DictionaryExtensions.cs
+++ Ampl.Core/DictionaryExtensions.cs
@@ -40,5 +40,23 @@ namespace Ampl.System
       return dictionary;
     }
 
+
+    /// <summary>
+    /// Gets the element with the specified key from the dictionary.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type or the valye.</typeparam>
+    /// <param name="dictionary">The dictionary.</param>
+    /// <param name="key">The key of the element to get.</param>
+    /// <returns>The element with the specified key, or the default value of <typeparamref name="TKey"/> if the
+    /// <paramref name="key"/> is not found.</returns>
+    public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+    {
+      Check.NotNull(dictionary, nameof(dictionary));
+
+      TValue value = default(TValue);
+      dictionary.TryGetValue(key, out value);
+      return value;
+    }
   }
 }
