8029633b58d04344e575d19c6c99a10946d2a70f
ghcentre <ghcentre@gmail.com>
2016-01-14 16:10:43 +0300

    EnumerableExtensions.ToEmptyIfNull and tests added.

--- Ampl.Core/EnumerableExtensions.cs
+++ Ampl.Core/EnumerableExtensions.cs
@@ -9,8 +9,19 @@ namespace Ampl.System
   /// <summary>
   /// Provides a set of <see langword="static"/> methods extending the <see cref="IEnumerable{T}"/> interface.
   /// </summary>
-  internal class EnumerableExtensions
+  public static class EnumerableExtensions
   {
-
+    /// <summary>
+    /// Converts the <see langword="null"/> reference to the <see cref="IEnumerable{T}"/> to the empty enumerable.
+    /// </summary>
+    /// <typeparam name="T">The type of the enumerable to convert.</typeparam>
+    /// <param name="source">The reference to convert.</param>
+    /// <returns>If the reference passed in <paramref name="source"/> is <see langword="null"/>, the method
+    /// returns the empty <see cref="IEnumerable{T}"/>. Otherwise, the method returns the same reference as passed
+    /// in <paramref name="source"/>.</returns>
+    public static IEnumerable<T> ToEmptyIfNull<T>(this IEnumerable<T> source)
+    {
+      return source ?? Enumerable.Empty<T>();
+    }
   }
 }
