f402cc83c5f06b7c746e193c90dda33475f1da03
George Harder <harder@ghcentre.com>
2017-05-04 07:18:39 +0300

    Added features to add to AMPL to separate directory.

---
+++ _2web.mvc/DateTimeValidationAttribute/TimeValidationAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Docexchange.Web
+{
+  public class TimeValidationAttribute : ValidationAttribute, IClientValidatable
+  {
+    #region IClientValidatable Members
+
+    public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+    {
+      yield return new ModelClientValidationRule() {
+        ErrorMessage = string.Format(
+          BootstrapTemplatesResources.BootstrapTemplatesResources.ClientDataTypeModelValidatorProvider_FieldMustBeTime,
+          metadata.GetDisplayName()),
+        ValidationType = "time"
+      };
+    }
+
+    #endregion
+
+    public override bool IsValid(object value)
+    {
+      if(value == null)
+      {
+        return true;
+      }
+
+      DateTime time;
+      if(!DateTime.TryParse(value.ToString(), out time))
+      {
+        return false;
+      }
+      return true;
+    }
+  }
+}
