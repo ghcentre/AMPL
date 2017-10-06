8f7850ef327f1ef7e82e19d55ae6ed2e49ae8edd
George Harder <harder@ghcentre.com>
2017-10-06 10:06:23 +0300

    VS2017

---
+++ Ampl.System/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace Ampl.System
+{
+  public static class EnumExtensions
+  {
+    private static DisplayAttribute GetFirstDisplayAttribute(Enum source)
+    {
+      return source?
+        .GetType()
+        .GetTypeInfo()
+        .GetDeclaredField(source.ToString())
+        //.First()
+        .GetCustomAttributes(typeof(DisplayAttribute), false)
+        .Select(x => x as DisplayAttribute)
+        .FirstOrDefault();
+    }
+
+    public static string GetDisplayName(this Enum source)
+    {
+      return GetFirstDisplayAttribute(source)?.GetName();
+    }
+
+    public static string GetDisplayDescription(this Enum source)
+    {
+      return GetFirstDisplayAttribute(source)?.GetDescription();
+    }
+  }
+}
