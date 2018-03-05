8c8d1c915caec4f340160b7e984e54cd0873baf5
George Harder <harder@ghcentre.com>
2018-03-05 10:10:52 +0300

    Another example added

---
+++ Ampl.Web.Mvc.EditorTemplates.Web/Models/ComplexEditor/CarEngine.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ampl.Web.Mvc.EditorTemplates.Web.Models.ComplexEditor
+{
+  public enum CarEngine
+  {
+    [Display(Name = "Unknown engine")]
+    Unknown = 0,
+
+    [Display(Name = "Gasoline engine")]
+    Gasoline = 1,
+
+    [Display(Name = "Diesel engine")]
+    Diesel = 2,
+
+    [Display(Name = "Pure electric")]
+    Electric = 3,
+
+    [Display(Name = "Electric + Gasoline Hybrid engine")]
+    HybridGasoline = 3,
+
+    [Display(Name = "Electric + Diesel Hybrid engine")]
+    HybridDiesel = 4,
+  }
+}
