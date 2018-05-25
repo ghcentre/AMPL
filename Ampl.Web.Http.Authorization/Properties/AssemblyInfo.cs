9f0ef9607e397d50be1612dddad12277e9528faa
George Harder <harder@ghcentre.com>
2018-05-25 11:49:17 +0300

    AMPL Web HTTP Authorization Package Created

---
+++ Ampl.Web.Http.Authorization/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AMPL Web HTTP Authorization")]
+[assembly: AssemblyDescription("AMPL extensions for ASP.NET Web API Authorization.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GHCentre")]
+[assembly: AssemblyProduct("GHC Advanced Multi-Purpose Library")]
+[assembly: AssemblyCopyright("Copyright © 2014-2018 George Harder's Centre.")]
+[assembly: AssemblyTrademark("AMPL")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4cc7feb1-2bd2-43ab-b65d-4cacd62b2b51")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.3.2")]
+[assembly: AssemblyFileVersion("3.3.2")]
