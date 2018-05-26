ed58a26479473853097066bbc589160409246e05
George Harder <harder@ghcentre.com>
2018-05-26 17:29:22 +0300

    Created package Ampl.Web.Http

---
+++ Ampl.Web.Http/ExposeExceptionMessageFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Ampl.System;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace Ampl.Web.Http
+{
+  public class ExposeExceptionMessageFilterAttribute : ExceptionFilterAttribute
+  {
+    private readonly Type _exceptionType;
+    private readonly HttpStatusCode _statusCode;
+
+    public ExposeExceptionMessageFilterAttribute(Type exceptionType, HttpStatusCode statusCode)
+    {
+      _exceptionType = Check.NotNull(exceptionType, nameof(exceptionType));
+      _statusCode = statusCode;
+    }
+
+    public override void OnException(HttpActionExecutedContext context)
+    {
+      if(_exceptionType.IsAssignableFrom(context.Exception.GetType()))
+      {
+        context.Response = context.Request.CreateErrorResponse(_statusCode, context.Exception.Message);
+      }
+    }
+  }
+}
---
+++ Ampl.Web.Http/ModelStateException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Http.ModelBinding;
+
+namespace Ampl.Web.Http
+{
+  public class ModelStateException : InvalidOperationException
+  {
+    public ModelStateDictionary ModelState { get; private set; }
+
+    public ModelStateException() : base()
+    { }
+
+    public ModelStateException(string message) : base(message)
+    { }
+
+    public ModelStateException(string message, ModelStateDictionary modelState) : base(message)
+    {
+      ModelState = modelState;
+    }
+  }
+}
