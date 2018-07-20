f47e6732b96f8cff8b8b2c8bda4378d734697210
George Harder <harder@ghcentre.com>
2018-07-20 20:25:37 +0300

    4.6.0, package update

--- Ampl.Configuration/AppConfig.cs
+++ Ampl.Configuration/AppConfig.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using Ampl.System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ampl.System;
 using System.Reflection;
 
 namespace Ampl.Configuration
