bee5a88c0817d9f5820b929e476bbb7fe6b801ad
George Harder <harder@ghcentre.com>
2019-03-03 23:12:11 +0300

    All in Ampl.System.Tests moved to Ampl.Core.Tests.

--- Ampl.Core.Tests/Check_Tests.cs
+++ Ampl.Core.Tests/Check_Tests.cs
@@ -1,91 +1,86 @@
-﻿using Ampl.System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ampl.Core.Tests
 {
-  [TestFixture]
-  public class Check_Tests
-  {
-    [Test]
-    public void Check_NotNull_NullArg_Throws()
+    [TestFixture]
+    public class Check_Tests
     {
-      // arrange
-      string arg = null;
-      // act-assert
-      Assert.Throws<ArgumentNullException>(() => Check.NotNull(arg));
-    }
+        [Test]
+        public void Check_NotNull_NullArg_Throws()
+        {
+            // arrange
+            string arg = null;
+            // act-assert
+            Assert.Throws<ArgumentNullException>(() => Check.NotNull(arg));
+        }
 
-    [Test]
-    public void Check_NotNull_NotNullArg_ReturnsSameReference()
-    {
-      string arg = "Not null string";
-      string result = Check.NotNull(arg);
-      Assert.That(result, Is.SameAs(arg));
-    }
+        [Test]
+        public void Check_NotNull_NotNullArg_ReturnsSameReference()
+        {
+            string arg = "Not null string";
+            string result = Check.NotNull(arg);
+            Assert.That(result, Is.SameAs(arg));
+        }
 
-    [Test]
-    public void Check_NotNull_NullArgWithParamName_ThrowsWithParamNameInMessage()
-    {
-      // arrange
-      string arg = null;
-      try
-      {
-        // act
-        Check.NotNull(arg, "ThisIsTheArg");
-      }
-      catch(ArgumentNullException e)
-      {
-        // assert
-        Assert.That(e.Message, Does.Contain("ThisIsTheArg"));
-      }
-    }
+        [Test]
+        public void Check_NotNull_NullArgWithParamName_ThrowsWithParamNameInMessage()
+        {
+            // arrange
+            string arg = null;
+            try
+            {
+                // act
+                Check.NotNull(arg, "ThisIsTheArg");
+            }
+            catch(ArgumentNullException e)
+            {
+                // assert
+                Assert.That(e.Message, Does.Contain("ThisIsTheArg"));
+            }
+        }
 
-    [Test]
-    public void Check_NotNullOrEmptyString_NullArg_ThrowsArgumentNullException()
-    {
-      // arrange
-      string arg = null;
-      // act-assert
-      Assert.Throws<ArgumentNullException>(() => Check.NotNullOrEmptyString(arg));
-    }
+        [Test]
+        public void Check_NotNullOrEmptyString_NullArg_ThrowsArgumentNullException()
+        {
+            // arrange
+            string arg = null;
+            // act-assert
+            Assert.Throws<ArgumentNullException>(() => Check.NotNullOrEmptyString(arg));
+        }
 
-    [Test]
-    public void Check_NotNullOrEmptyString_EmptyArg_ThrowsArgumentException()
-    {
-      // arrange
-      string arg = string.Empty;
-      // act-assert
-      Assert.Throws<ArgumentException>(() => Check.NotNullOrEmptyString(arg));
-    }
+        [Test]
+        public void Check_NotNullOrEmptyString_EmptyArg_ThrowsArgumentException()
+        {
+            // arrange
+            string arg = string.Empty;
+            // act-assert
+            Assert.Throws<ArgumentException>(() => Check.NotNullOrEmptyString(arg));
+        }
 
-    [Test]
-    public void Check_NotNullOrEmptyString_NullArgWithParamName_ThrowsWithParamNameInMessage()
-    {
-      // arrange
-      string arg = null;
-      try
-      {
-        // act
-        Check.NotNullOrEmptyString(arg, "ThisIsArgumentName");
-      }
-      catch(Exception e)
-      {
-        // assert
-        Assert.That(e.Message, Does.Contain("ThisIsArgumentName"));
-      }
-    }
+        [Test]
+        public void Check_NotNullOrEmptyString_NullArgWithParamName_ThrowsWithParamNameInMessage()
+        {
+            // arrange
+            string arg = null;
+            try
+            {
+                // act
+                Check.NotNullOrEmptyString(arg, "ThisIsArgumentName");
+            }
+            catch(Exception e)
+            {
+                // assert
+                Assert.That(e.Message, Does.Contain("ThisIsArgumentName"));
+            }
+        }
 
-    [Test]
-    public void Check_NotNullOrEmptyString_NotNullArg_ReturnsSameReference()
-    {
-      string arg = "This is some argument";
-      string result = Check.NotNullOrEmptyString(arg, "ThisIsArgument");
-      Assert.That(result, Is.SameAs(arg));
+        [Test]
+        public void Check_NotNullOrEmptyString_NotNullArg_ReturnsSameReference()
+        {
+            string arg = "This is some argument";
+            string result = Check.NotNullOrEmptyString(arg, "ThisIsArgument");
+            Assert.That(result, Is.SameAs(arg));
+        }
     }
-  }
 }
--- Ampl.Core.Tests/DictionaryExtensions_Tests.cs
+++ Ampl.Core.Tests/DictionaryExtensions_Tests.cs
@@ -1,84 +1,80 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ampl.System;
 
 namespace Ampl.Core.Tests
 {
-  [TestFixture]
-  public class DictionaryExtensions_Tests
-  {
-    [Test]
-    public void IncludeObjectProperties_NullThis_Throws()
+    [TestFixture]
+    public class DictionaryExtensions_Tests
     {
-      // arrange
-      Dictionary<string, object> dic = null;
-      var anon = new { IntProp = 5, StringProp = "String" };
-      // act-assert
-      Assert.Throws<ArgumentNullException>(() => dic.IncludeObjectProperties(anon));
-    }
+        [Test]
+        public void IncludeObjectProperties_NullThis_Throws()
+        {
+            // arrange
+            Dictionary<string, object> dic = null;
+            var anon = new { IntProp = 5, StringProp = "String" };
+            // act-assert
+            Assert.Throws<ArgumentNullException>(() => dic.IncludeObjectProperties(anon));
+        }
 
-    [Test]
-    public void IncludeObjectPropertis_NullArg_Throws()
-    {
-      // arrange
-      var dic = new Dictionary<string, object>();
-      object anon = null;
-      // act-assert
-      Assert.Throws<ArgumentNullException>(() => dic.IncludeObjectProperties(anon));
-    }
+        [Test]
+        public void IncludeObjectPropertis_NullArg_Throws()
+        {
+            // arrange
+            var dic = new Dictionary<string, object>();
+            object anon = null;
+            // act-assert
+            Assert.Throws<ArgumentNullException>(() => dic.IncludeObjectProperties(anon));
+        }
 
-    [Test]
-    public void IncludeObjectProperties_Add_AddsAllProperties()
-    {
-      // arrange
-      var dic = new Dictionary<string, object>();
-      var anon = new { IntProp = 5, StringProp = "String" };
-      // act
-      dic.IncludeObjectProperties(anon);
-      // assert
-      Assert.That(dic.Count, Is.EqualTo(2));
-    }
+        [Test]
+        public void IncludeObjectProperties_Add_AddsAllProperties()
+        {
+            // arrange
+            var dic = new Dictionary<string, object>();
+            var anon = new { IntProp = 5, StringProp = "String" };
+            // act
+            dic.IncludeObjectProperties(anon);
+            // assert
+            Assert.That(dic.Count, Is.EqualTo(2));
+        }
 
-    [Test]
-    public void IncludeObjectProperties_Add_ReferencesSameObjects()
-    {
-      // arrange
-      var dic = new Dictionary<string, object>();
-      var anon = new { IntProp = 5, StringProp = "String" };
-      // act
-      dic.IncludeObjectProperties(anon);
-      // assert
-      Assert.That((int)dic["IntProp"], Is.EqualTo(5));
-      Assert.That((string)dic["StringProp"], Is.SameAs(anon.StringProp));
-    }
+        [Test]
+        public void IncludeObjectProperties_Add_ReferencesSameObjects()
+        {
+            // arrange
+            var dic = new Dictionary<string, object>();
+            var anon = new { IntProp = 5, StringProp = "String" };
+            // act
+            dic.IncludeObjectProperties(anon);
+            // assert
+            Assert.That((int)dic["IntProp"], Is.EqualTo(5));
+            Assert.That((string)dic["StringProp"], Is.SameAs(anon.StringProp));
+        }
 
-    [Test]
-    public void IncludeObjectProperties_AddToNonEmpty_Replaces()
-    {
-      // arrange
-      var dic = new Dictionary<string, object>() { { "StringProp", "OldString" } };
-      var anon = new { IntProp = 5, StringProp = "String" };
-      // act
-      dic.IncludeObjectProperties(anon);
-      // assert
-      Assert.That(dic.Count, Is.EqualTo(2));
-      Assert.That((string)dic["StringProp"], Is.SameAs(anon.StringProp));
-      Assert.That((string)dic["StringProp"], Is.Not.EqualTo("OldString"));
-    }
+        [Test]
+        public void IncludeObjectProperties_AddToNonEmpty_Replaces()
+        {
+            // arrange
+            var dic = new Dictionary<string, object>() { { "StringProp", "OldString" } };
+            var anon = new { IntProp = 5, StringProp = "String" };
+            // act
+            dic.IncludeObjectProperties(anon);
+            // assert
+            Assert.That(dic.Count, Is.EqualTo(2));
+            Assert.That((string)dic["StringProp"], Is.SameAs(anon.StringProp));
+            Assert.That((string)dic["StringProp"], Is.Not.EqualTo("OldString"));
+        }
 
 
-    [Test]
-    public void GetValueOrDefault_NullThis_Throws()
-    {
-      Dictionary<string, string> arg = null;
-      Assert.Throws<ArgumentNullException>(() => arg.GetValueOrDefault("test"));
-    }
+        [Test]
+        public void GetValueOrDefault_NullThis_Throws()
+        {
+            Dictionary<string, string> arg = null;
+            Assert.Throws<ArgumentNullException>(() => arg.GetValueOrDefault("test"));
+        }
 
-    private Dictionary<int, string> _errorMessages = new Dictionary<int, string>() {
+        private Dictionary<int, string> _errorMessages = new Dictionary<int, string>() {
       { 0, "The operation completed successfully" },
       { 1, "Incorrect function" },
       { 2, "File not found" },
@@ -87,33 +83,33 @@ namespace Ampl.Core.Tests
       { 5, "Access denied" }
     };
 
-    [Test]
-    public void GetValueOrDefault_ExistingKey_Returns()
-    {
-      int key = 3;
-      var result = _errorMessages.GetValueOrDefault(key);
-      Assert.That(result, Is.EqualTo("Path not found"));
-    }
+        [Test]
+        public void GetValueOrDefault_ExistingKey_Returns()
+        {
+            int key = 3;
+            var result = _errorMessages.GetValueOrDefault(key);
+            Assert.That(result, Is.EqualTo("Path not found"));
+        }
 
-    [Test]
-    public void GetValueOrDefault_NonExistingKey_ReturnsNull()
-    {
-      int key = 1000;
-      var result = _errorMessages.GetValueOrDefault(key);
-      Assert.That(result, Is.Null);
-    }
+        [Test]
+        public void GetValueOrDefault_NonExistingKey_ReturnsNull()
+        {
+            int key = 1000;
+            var result = _errorMessages.GetValueOrDefault(key);
+            Assert.That(result, Is.Null);
+        }
 
-    [Test]
-    public void GetValueOrDefault_ValueTypesNonExistingKey_ReturnsDefault()
-    {
-      var dictionary = new Dictionary<string, int>() {
-        ["one"] = 1,
-        ["two"] = 2,
-        ["three"] = 3
-      };
-      string key = "four";
-      int result = dictionary.GetValueOrDefault(key);
-      Assert.That(result, Is.EqualTo(default(int)));
+        [Test]
+        public void GetValueOrDefault_ValueTypesNonExistingKey_ReturnsDefault()
+        {
+            var dictionary = new Dictionary<string, int>() {
+                ["one"] = 1,
+                ["two"] = 2,
+                ["three"] = 3
+            };
+            string key = "four";
+            int result = dictionary.GetValueOrDefault(key);
+            Assert.That(result, Is.EqualTo(default(int)));
+        }
     }
-  }
 }
