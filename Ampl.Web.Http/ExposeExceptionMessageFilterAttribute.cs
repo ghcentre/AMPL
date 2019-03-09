b3abbe69dbb7aeecfa1cf91057d096f8e7b271cd
George Harder <harder@ghcentre.com>
2019-03-09 11:43:14 +0300

    Package updated.

--- Ampl.Web.Http/ExposeExceptionMessageFilterAttribute.cs
+++ Ampl.Web.Http/ExposeExceptionMessageFilterAttribute.cs
@@ -7,22 +7,22 @@ using System.Web.Http.Filters;
 namespace Ampl.Web.Http
 {
     public class ExposeExceptionMessageFilterAttribute : ExceptionFilterAttribute
-  {
-    private readonly Type _exceptionType;
-    private readonly HttpStatusCode _statusCode;
-
-    public ExposeExceptionMessageFilterAttribute(Type exceptionType, HttpStatusCode statusCode)
     {
-      _exceptionType = Check.NotNull(exceptionType, nameof(exceptionType));
-      _statusCode = statusCode;
-    }
+        private readonly Type _exceptionType;
+        private readonly HttpStatusCode _statusCode;
 
-    public override void OnException(HttpActionExecutedContext context)
-    {
-      if(_exceptionType.IsAssignableFrom(context.Exception.GetType()))
-      {
-        context.Response = context.Request.CreateErrorResponse(_statusCode, context.Exception.Message);
-      }
+        public ExposeExceptionMessageFilterAttribute(Type exceptionType, HttpStatusCode statusCode)
+        {
+            _exceptionType = Check.NotNull(exceptionType, nameof(exceptionType));
+            _statusCode = statusCode;
+        }
+
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if(_exceptionType.IsAssignableFrom(context.Exception.GetType()))
+            {
+                context.Response = context.Request.CreateErrorResponse(_statusCode, context.Exception.Message);
+            }
+        }
     }
-  }
 }
