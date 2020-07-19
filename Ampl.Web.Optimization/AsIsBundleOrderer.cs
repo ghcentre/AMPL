86c65c5b0033d5fb98df571349835dea5a935f0b
George Harder <harder@ghcentre.com>
2020-07-19 06:23:31 +0300

    Package updates, source code formatting fix

--- Ampl.Web.Optimization/AsIsBundleOrderer.cs
+++ Ampl.Web.Optimization/AsIsBundleOrderer.cs
@@ -3,11 +3,11 @@ using System.Web.Optimization;
 
 namespace Ampl.Web.Optimization
 {
-  internal class AsIsBundleOrderer : IBundleOrderer
-  {
-    public virtual IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+    internal class AsIsBundleOrderer : IBundleOrderer
     {
-      return files;
+        public virtual IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
     }
-  }
 }
--- Ampl.Web.Optimization/BundleExtensions.cs
+++ Ampl.Web.Optimization/BundleExtensions.cs
@@ -3,11 +3,11 @@
 namespace Ampl.Web.Optimization
 {
     public static class BundleExtensions
-  {
-    public static Bundle ForceOrder(this Bundle bundle)
     {
-      bundle.Orderer = new AsIsBundleOrderer();
-      return bundle;
+        public static Bundle ForceOrder(this Bundle bundle)
+        {
+            bundle.Orderer = new AsIsBundleOrderer();
+            return bundle;
+        }
     }
-  }
 }
