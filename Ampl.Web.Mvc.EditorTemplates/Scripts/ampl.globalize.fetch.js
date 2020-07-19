8897fa3a5cffbdac8dd2f672536639e53a5e6dfa
George Harder <harder@ghcentre.com>
2020-07-19 09:11:43 +0300

    Package updates, source code formatting fix.

--- Ampl.Web.Mvc.EditorTemplates/Scripts/ampl.globalize.fetch.js
+++ Ampl.Web.Mvc.EditorTemplates/Scripts/ampl.globalize.fetch.js
@@ -1,17 +1,18 @@
-﻿$.when(
-    $.getJSON("/Scripts/cldr/supplemental/likelySubtags.json"),
-    $.getJSON("/Scripts/cldr/supplemental/numberingSystems.json"),
-    $.getJSON("/Scripts/cldr/supplemental/timeData.json"),
-    $.getJSON("/Scripts/cldr/supplemental/weekData.json"),
+﻿$
+    .when(
+        $.getJSON("/Scripts/cldr/supplemental/likelySubtags.json"),
+        $.getJSON("/Scripts/cldr/supplemental/numberingSystems.json"),
+        $.getJSON("/Scripts/cldr/supplemental/timeData.json"),
+        $.getJSON("/Scripts/cldr/supplemental/weekData.json"),
 
-    $.getJSON("/Scripts/cldr/main/en/numbers.json"),
-    $.getJSON("/Scripts/cldr/main/en/ca-gregorian.json"),
-    $.getJSON("/Scripts/cldr/main/en/timeZoneNames.json"),
+        $.getJSON("/Scripts/cldr/main/en/numbers.json"),
+        $.getJSON("/Scripts/cldr/main/en/ca-gregorian.json"),
+        $.getJSON("/Scripts/cldr/main/en/timeZoneNames.json"),
 
-    $.getJSON("/Scripts/cldr/main/ru/numbers.json"),
-    $.getJSON("/Scripts/cldr/main/ru/ca-gregorian.json"),
-    $.getJSON("/Scripts/cldr/main/ru/timeZoneNames.json")
-)
+        $.getJSON("/Scripts/cldr/main/ru/numbers.json"),
+        $.getJSON("/Scripts/cldr/main/ru/ca-gregorian.json"),
+        $.getJSON("/Scripts/cldr/main/ru/timeZoneNames.json")
+    )
     .then(
         function()
         {
