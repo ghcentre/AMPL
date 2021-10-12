7c01b5978b23070fc29bc1d73ff84b9dbe9dae45
George Harder <harder@ghcentre.com>
2021-10-12 12:18:39 +0300

    Comments added.

--- Ampl.Core/Guid/CompactGuid.cs
+++ Ampl.Core/Guid/CompactGuid.cs
@@ -2,11 +2,22 @@
 
 namespace Ampl.Core
 {
+    /// <summary>
+    /// Contains a set of <see langword="static"/> methots that converts the <see cref="Guid"/> compact string representation
+    /// to the Guid instance.
+    /// </summary>
     public static class CompactGuid
     {
         private const int _shortGuidLength = 22;
         private const string _terminator = "==";
 
+        /// <summary>
+        /// Converts the compact string representation of a GUID to the equivalent <see cref="Guid"/> structure.
+        /// </summary>
+        /// <param name="stringValue">The string to convert.</param>
+        /// <returns>A structure that contains the value that was parsed.</returns>
+        /// <exception cref="ArgumentNullException">Input is <see langword="null"/>.</exception>
+        /// <exception cref="FormatException">Input is not in a recognized format.</exception>
         public static Guid Parse(string stringValue)
         {
             _ = stringValue ?? throw new ArgumentNullException(nameof(stringValue));
