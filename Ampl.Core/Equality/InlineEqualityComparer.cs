a71e289443a09fdd70af9493a32c6b5e895c43f5
George Harder <harder@ghcentre.com>
2021-10-12 15:15:38 +0300

    InlineEqualityComparer added.

---
+++ Ampl.Core/Equality/InlineEqualityComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ampl.Core
+{
+    /// <summary>
+    /// Implements the <see cref="IEqualityComparer{T}"/> allowing to specify equality comparer and hash code generator
+    /// as constructor arguments.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to compare</typeparam>
+    public class InlineEqualityComparer<T> : EqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _equals;
+        private readonly Func<T, int> _getHashCode;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InlineEqualityComparer{T}"/> class.
+        /// </summary>
+        /// <param name="equals">The delegate implementing equality comparer.</param>
+        /// <param name="getHashCode">The delegate implementing hash code generator.</param>
+        public InlineEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            _equals = equals ?? throw new ArgumentNullException(nameof(equals));
+            _getHashCode = getHashCode ?? throw new ArgumentNullException(nameof(getHashCode));
+        }
+
+
+        /// <inheritdoc />
+        public override bool Equals(T x, T y) => _equals(x, y);
+
+        /// <inheritdoc />
+        public override int GetHashCode(T obj) => _getHashCode(obj);
+    }
+}
---
+++ tests/Ampl.Core.Tests/InlineEqualityComparer_Tests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ampl.Core.Tests
+{
+    [TestFixture]
+    public class InlineEqualityComparer_Tests
+    {
+        [Test]
+        public void Ctor_EqualsIsNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new InlineEqualityComparer<int>(null, x => x.GetHashCode()));
+        }
+
+        [Test]
+        public void Ctor_GethashcodeEqualsNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new InlineEqualityComparer<int>((x, y) => x.Equals(y), null));
+        }
+
+        [Test]
+        public void GroupBy_CaseInsensitiveString_GroupsCaseInsensitive()
+        {
+            // arrange
+            var items = new(string, int)[]
+            {
+                ("One", 1),
+                ("Two", 2),   ("two", 22),
+                ("Three", 3), ("Three", 33), ("three", 333)
+            };
+
+            // act
+            var comparer = new InlineEqualityComparer<string>(
+                (x, y) => x.Equals(y, StringComparison.OrdinalIgnoreCase),
+                x => x.ToLower().GetHashCode());
+            var result = items.GroupBy(x => x.Item1, comparer);
+            var ones = result.First(x => x.Key.ToLower() == "one");
+            var twos = result.First(x => x.Key.ToLower() == "two");
+            var threes = result.First(x => x.Key.ToLower() == "three");
+
+            // assert
+            Assert.That(ones.Count(), Is.EqualTo(1));
+            Assert.That(twos.Count(), Is.EqualTo(2));
+            Assert.That(threes.Count(), Is.EqualTo(3));
+        }
+    }
+}
