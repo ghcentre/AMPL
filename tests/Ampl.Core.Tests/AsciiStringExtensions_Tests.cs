54d91dfe7f00d7625f859cbf75e0b73b9891e78b
GHCentre <ghcentre@gmail.com>
2022-02-15 18:27:07 +0300

    FoldToAscii (remove diactrics) added to string extensions.

---
+++ tests/Ampl.Core.Tests/AsciiStringExtensions_Tests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace Ampl.Core.Tests
+{
+    [TestFixture]
+    public class AsciiStringExtensions_Tests
+    {
+        #region FoldToAscii
+
+        [Test]
+        public void FoldToAscii_Null_ReturnsNull()
+        {
+            var arg = (string)null!;
+            var result = arg.FoldToAscii();
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void FoldToAscii_Empty_ReturnsEmpty()
+        {
+            var arg = string.Empty;
+            var result = arg.FoldToAscii();
+            Assert.That(result, Is.EqualTo(string.Empty));
+        }
+
+        [TestCase("hello", "hello")]
+        [TestCase("Hello", "Hello")]
+        [TestCase("123", "123")]
+        [TestCase("Wüsthof Straße", "Wusthof Strasse")]
+        [TestCase("ÁÂÃÄÅ Ç ÈÉ àáâãäå èéêë ìíîï òóôõ", "AAAAA C EE aaaaaa eeee iiii oooo")]
+        [TestCase("Пятый-подъезд-Ёлкина", "Пятый-подъезд-Ёлкина")]
+        public void FoldToAscii_Expected(string arg, string expected)
+        {
+            var result = arg.FoldToAscii();
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        #endregion
+    }
+}
