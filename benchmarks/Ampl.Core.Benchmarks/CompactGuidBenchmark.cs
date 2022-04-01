5d4dbe544d08a9f2e7e54992efde79b7860984fe
GHCentre <ghcentre@gmail.com>
2022-04-01 09:30:00 +0300

    Updated benchmarks.

--- benchmarks/Ampl.Core.Benchmarks/CompactGuidBenchmark.cs
+++ benchmarks/Ampl.Core.Benchmarks/CompactGuidBenchmark.cs
@@ -8,11 +8,11 @@ public class CompactGuidBenchmark
     private static readonly Guid _guid = new("48e42ef1-ddcf-4e1f-9f93-d31ab170dd03");
     private static readonly string _compactGuid = "8S7kSM_dH06fk9MasXDdAw";
 
-    [Benchmark]
-    public string ToCompactGuidV1()
-    {
-        return _guid.ToCompactStringUnoptimized();
-    }
+    //[Benchmark]
+    //public string ToCompactGuidV1()
+    //{
+    //    return _guid.ToCompactStringUnoptimized();
+    //}
 
     [Benchmark]
     public string ToCompactGuidV2()
@@ -20,11 +20,11 @@ public class CompactGuidBenchmark
         return _guid.ToCompactString();
     }
 
-    [Benchmark]
-    public Guid FromCompactGuidV1()
-    {
-        return CompactGuid.ParseUnoptimized(_compactGuid);
-    }
+    //[Benchmark]
+    //public Guid FromCompactGuidV1()
+    //{
+    //    return CompactGuid.ParseUnoptimized(_compactGuid);
+    //}
 
     [Benchmark]
     public Guid FromCompactGuidV2()
